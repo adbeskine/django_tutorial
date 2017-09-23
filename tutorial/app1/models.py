d737c30981c1907dfcc766efdd043b491ef21b51
Alexander Beskine <a.d.beskine@outlook.com>
2017-09-23 16:56:46 +0100

    added string representations and requirements.txt

--- tutorial/app1/models.py
+++ tutorial/app1/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
-	pub_date = models.DateTimeField('date published')
+	pub_date = models.DateTimeField('date published') 
+
+	def __str__(self):
+		return 'question: ' + self.question_text + ' date: ' + str(self.pub_date) 
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
 
+	def __str__(self):
+		return 'question: ' + self.question + '/n choice text: ' + self.choice_text + '/n votes: ' + self.votes
 # Create your models here.
