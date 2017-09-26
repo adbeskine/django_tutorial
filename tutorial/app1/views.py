e8834743d3bb1fd7c33158a0bc456c2a9e62c412
Alexander Beskine <a.d.beskine@outlook.com>
2017-09-26 15:40:13 +0100

    first dynamic url

--- tutorial/app1/views.py
+++ tutorial/app1/views.py
@@ -5,4 +5,7 @@ from django.http import HttpResponse
 def index(request):
 	return HttpResponse("You are in app1's index, let's learn Django!")
 
+def detail(request, question_id):
+	return HttpResponse("You're looking at question %s." % question_id)
+
 # Create your views here.
