233990747e49719f4bfe523ca13aa73c4811999f
Alexander Beskine <a.d.beskine@outlook.com>
2017-09-26 11:59:38 +0100

    registered app1 Question to the admin

--- tutorial/app1/models.py
+++ tutorial/app1/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published') 
 
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 	def __str__(self):
-		return 'question: ' + self.question_text + ' date: ' + str(self.pub_date) 
+		return 'question: ' + self.question_text + ' |date: ' + str(self.pub_date) 
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -13,5 +18,5 @@ class Choice(models.Model):
 	votes = models.IntegerField(default=0)
 
 	def __str__(self):
-		return 'question: ' + self.question + '/n choice text: ' + self.choice_text + '/n votes: ' + self.votes
+		return 'choice text: ' + self.choice_text + ' |votes: ' + str(self.votes) #puttin self.question triggers a typerror
 # Create your models here.
