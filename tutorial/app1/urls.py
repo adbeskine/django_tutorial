4b84b0968f4f23a9b0115f98d07049475e260d41
Alexander Beskine <a.d.beskine@outlook.com>
2017-09-28 14:06:06 +0100

    namespaced the urls

--- tutorial/app1/urls.py
+++ tutorial/app1/urls.py
@@ -3,6 +3,8 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'app1'
+
 urlpatterns = [
 url(r'^$', views.index, name='app1index'),
 url(r'^indexgenvar/$', views.index_generated_variables, name='indexgenvar'),
