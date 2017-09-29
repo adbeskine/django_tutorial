f0a3a5efbe6cf0146bd2cb2d73270a6986a9e876
Alexander Beskine <a.d.beskine@outlook.com>
2017-09-29 17:09:00 +0100

    re-organized code for clarity

--- tutorial/app1/urls.py
+++ tutorial/app1/urls.py
@@ -6,13 +6,23 @@ from . import views
 app_name = 'app1'
 
 urlpatterns = [
+# --------------- BASIC URLS ------------------------------------- #
 url(r'^$', views.index, name='app1index'),
+url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail')
+
+# ---------------------- FUNCTIONAL -------------------------- #
 url(r'^indexgenvar/$', views.index_generated_variables, name='indexgenvar'),
 url(r'^indexhtmllong/$', views.index_html_longhand, name='indexhtmllong'),
-url(r'^indexhtmlshort/$', views.index_html_shorthand, name='indexhtmlshort'),
-url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
 url(r'^404enabled/(?P<question_id>[0-9]+)/$', views.detail_with_404, name='detailw404'),
+
+#------------------------SHORTCUTS-----------------------------#
+url(r'^indexhtmlshort/$', views.index_html_shorthand, name='indexhtmlshort'),
 url(r'^404shortcut/(?P<question_id>[0-9]+)/$', views.detail_404_shortcut, name='detail404shortcut'),
+
+#--------------------------------OTHER------------------------#
 url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote')
+url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+#-------------------------GENERIC VIEWS------------------------#
 ]
+
--- tutorial/app1/views.py
+++ tutorial/app1/views.py
@@ -7,11 +7,32 @@ from django.shortcuts import render, get_object_or_404
 from .models import Choice, Question
 from django.urls import reverse
 
+# --------------------------BASIC VIEWS -----------------------------------#
+
 def index(request):
 	return HttpResponse("You are in app1's index, let's learn Django!")
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at question %s." % question_id)
+
+
+#----------------------------FUNCTIONAL VIEWS------------------------------#
+
+def index_generated_variables(request):
+	latest_question_list = Question.objects.order_by('pub_date')[:5]
+	output = ' '.join([q.question_text + '|||' for q in latest_question_list])
+	return HttpResponse(output)
+
+# 1. get the template
+# 2. define any variables used in the template (context)
+# 3. normal httpresponse with render method
+def index_html_longhand(request):
+	latest_question_list = Question.objects.order_by('pub_date')[:5] 
+	template = loader.get_template('app1/index.html')
+	context={
+		'latest_question_list': latest_question_list,
+	}
+	return HttpResponse(template.render(context, request))
 
 def detail_with_404(request, question_id):
 	try:
@@ -20,9 +41,22 @@ def detail_with_404(request, question_id):
 		raise Http404("Question does not exist")
 	return render(request, 'app1/detail.html', {'question': question})
 
+#----------------------------------SHORTCUTS-----------------------------------------#
+
+# 1. define variables
+# 2. pass it into context format (note you can skip this and put a dictionary directly into the render method in step 3)
+# 3. render(request, template, context)
+def index_html_shorthand(request):
+	latest_question_list=Question.objects.order_by('pub_date')[:5]
+	context = {'latest_question_list' : latest_question_list,}
+	return render(request, 'app1/index.html', context)
+
 def detail_404_shortcut(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
 	return render(request, 'app1/detail.html', {'question': question})
+
+
+#---------------------------------------OTHER-------------------------------------#
 
 def results(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
@@ -43,30 +77,8 @@ def vote(request, question_id):
 		selected_choice.save()
 		return HttpResponseRedirect(reverse('app1:results', args=(question.id,)))
 
-def index_generated_variables(request):
-	latest_question_list = Question.objects.order_by('pub_date')[:5]
-	output = ' '.join([q.question_text + '|||' for q in latest_question_list])
-	return HttpResponse(output)
+#-----------------------------------------------------------------------------------#
 
-
-# 1. get the template
-# 2. define any variables used in the template (context)
-# 3. normal httpresponse with render method
-def index_html_longhand(request):
-	latest_question_list = Question.objects.order_by('pub_date')[:5]
-	template = loader.get_template('app1/index.html')
-	context={
-		'latest_question_list': latest_question_list,
-	}
-	return HttpResponse(template.render(context, request))
-
-# 1. define variables
-# 2. pass it into context format (note you can skip this and put a dictionary directly into the render method in step 3)
-# 3. render(request, template, context)
-def index_html_shorthand(request):
-	latest_question_list=Question.objects.order_by('pub_date')[:5]
-	context = {'latest_question_list' : latest_question_list,}
-	return render(request, 'app1/index.html', context)
 
 
 
